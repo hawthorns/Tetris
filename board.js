e2cdfde8f890f5f2ffdad491f2a4fa8e4d5f5b2c
kerry <kerrysz.sk@gmail.com>
2015-11-20 18:53:17 +0800

    demo

--- board.js
+++ board.js
@@ -1,29 +1,37 @@
 var BLANK = 46 // '.';
 var BOX_SIZE = 20;
-var BOARD_MARGIN = 50;
+var BOARD_MARGIN = 80;
 var TEMPLATE_WIDTH = 5
 var TEMPLATE_HEIGHT = 5
+var interval = 1000;
 
 function board(boardWidth, boardHeight, ctx) {
   this.boardWidth = boardWidth;
   this.boardHeight = boardHeight;
   this.graphics = ctx;
   this.score = 0;
-  this.clolorMap = ["white", "pink", "red", "blue", "yellow", "green"];
+  this.clolorMap = ["white", "rgb(185, 185, 185)", "red", 
+        "rgb(175,  20,  20)", "green", "rgb( 20, 175,  20)",
+        "rgb( 20,  20, 175)", "yellow", "rgb(175, 175,  20)"];
   this.fallingSpirit = null;
   this.boardMap = null;
+  this.movingLeft = false;
+  this.movingRight = false;
+  this.movingDown = false;
+  this.gamePaused = false;
+  this.gameOver = false;
 }
 
 board.prototype.init = function() {
-  var boardMap = new Array();
-	for (var x = 0; x < this.boardWidth;  x++) {
-    boardMap[x] = new Array();
-		for (var y = 0; y < this.boardHeight; y++) {
-      boardMap[x][y] = BLANK;
-		}
-  }
-  this.boardMap = boardMap; 
-  var S_SPIRIT_TEMPLATE = 
+    var boardMap = new Array();
+    for (var x = 0; x < this.boardWidth;  x++) {
+        boardMap[x] = new Array();
+        for (var y = 0; y < this.boardHeight; y++) {
+            boardMap[x][y] = BLANK;
+        }
+    }
+  this.boardMap = boardMap;
+  var S_SPIRIT_TEMPLATE =
   [['.....',
     '.....',
     '..OO.',
@@ -35,7 +43,7 @@ board.prototype.init = function() {
     '...O.',
   '.....']]
 
-  var Z_SPIRIT_TEMPLATE = 
+  var Z_SPIRIT_TEMPLATE =
   [['.....',
     '.....',
     '.OO..',
@@ -47,7 +55,7 @@ board.prototype.init = function() {
     '.O...',
   '.....']]
 
-  var I_SPIRIT_TEMPLATE = 
+  var I_SPIRIT_TEMPLATE =
   [['..O..',
     '..O..',
     '..O..',
@@ -59,14 +67,14 @@ board.prototype.init = function() {
     '.....',
   '.....']]
 
-  var O_SPIRIT_TEMPLATE = 
+  var O_SPIRIT_TEMPLATE =
   [['.....',
     '.....',
     '.OO..',
     '.OO..',
   '.....']]
 
-  var J_SPIRIT_TEMPLATE = 
+  var J_SPIRIT_TEMPLATE =
   [['.....',
     '.O...',
     '.OOO.',
@@ -88,7 +96,7 @@ board.prototype.init = function() {
     '.OO..',
   '.....']]
 
-  var L_SPIRIT_TEMPLATE = 
+  var L_SPIRIT_TEMPLATE =
   [['.....',
     '...O.',
     '.OOO.',
@@ -110,7 +118,7 @@ board.prototype.init = function() {
     '..O..',
   '.....']]
 
-  var T_SPIRIT_TEMPLATE = 
+  var T_SPIRIT_TEMPLATE =
   [['.....',
     '..O..',
     '.OOO.',
@@ -144,69 +152,123 @@ board.prototype.init = function() {
   this.SPIRITS = SPIRITS;
 }
 
+
 board.prototype.drawBoard = function() {
-	for (var x = 0; x < this.boardWidth;  x++) {
-		for (var y = 0; y < this.boardHeight; y++) {
-			var color = this.boardMap[x][y];
-			if(color != BLANK) {
-				this.graphics.fillStyle = this.clolorMap[color];
-				this.graphics.fillRect(BOARD_MARGIN+x*BOX_SIZE, BOARD_MARGIN+y*BOX_SIZE, BOX_SIZE, BOX_SIZE); 
-			}
-		}
-	}
+    for (var x = 0; x < this.boardWidth;  x++) {
+        for (var y = 0; y < this.boardHeight; y++) {
+            var color = this.boardMap[x][y];
+            if(color != BLANK) {
+                this.graphics.fillStyle = this.clolorMap[color];
+                this.graphics.fillRect(BOARD_MARGIN+x*BOX_SIZE, BOARD_MARGIN+y*BOX_SIZE, BOX_SIZE, BOX_SIZE);
+            }
+        }
+    }
+}
 
-	this.graphics.strokeStyle = "#0000ff";
-	this.graphics.lineWidth = 1;
-	for (var x = 0; x <= this.boardHeight;  x++) {
-		this.graphics.moveTo(BOARD_MARGIN, x*BOX_SIZE+BOARD_MARGIN);
-		this.graphics.lineTo(this.boardWidth*BOX_SIZE+BOARD_MARGIN, x*BOX_SIZE+BOARD_MARGIN);
-	}
+board.prototype.drawBoardLine = function() {
+    this.graphics.strokeStyle = "rgb(0, 0, 155)";
+    this.graphics.beginPath();
+    for (var x = 0; x <= this.boardHeight;  x++) {
+        this.graphics.moveTo(BOARD_MARGIN, x*BOX_SIZE+BOARD_MARGIN);
+        this.graphics.lineTo(this.boardWidth*BOX_SIZE+BOARD_MARGIN, x*BOX_SIZE+BOARD_MARGIN);
+    }
 
-	for (var y = 0; y <= this.boardWidth; y++) {
-		this.graphics.moveTo(y*BOX_SIZE+BOARD_MARGIN, BOARD_MARGIN);
-		this.graphics.lineTo(y*BOX_SIZE+BOARD_MARGIN, this.boardHeight*BOX_SIZE+BOARD_MARGIN);
-	}
-	this.graphics.stroke();
-	this.graphics.lineWidth = 0;
+    for (var y = 0; y <= this.boardWidth; y++) {
+        this.graphics.moveTo(y*BOX_SIZE+BOARD_MARGIN, BOARD_MARGIN);
+        this.graphics.lineTo(y*BOX_SIZE+BOARD_MARGIN, this.boardHeight*BOX_SIZE+BOARD_MARGIN);
+    }
+    this.graphics.closePath();
+    this.graphics.stroke();
 }
 
 board.prototype.drawSpirit = function(spirit) {
-	var color = spirit.color;
-	this.graphics.fillStyle = this.clolorMap[color];
-	var px = BOARD_MARGIN + spirit.x*BOX_SIZE;
-	var py = BOARD_MARGIN + spirit.y*BOX_SIZE;
-	for (var x = 0; x < TEMPLATE_WIDTH;  x++) {
-		for (var y = 0; y < TEMPLATE_HEIGHT;  y++) {
-			if (this.SPIRITS[spirit.shape][spirit.rotation][y].charCodeAt(x) != BLANK) {
-				this.graphics.fillRect(px+x*BOX_SIZE, py+y*BOX_SIZE, BOX_SIZE, BOX_SIZE);
-			}
-		}
-	}
+    var color = spirit.color;
+    this.graphics.fillStyle = this.clolorMap[color];
+    var px = BOARD_MARGIN + spirit.x*BOX_SIZE;
+    var py = BOARD_MARGIN + spirit.y*BOX_SIZE;
+    for (var x = 0; x < TEMPLATE_WIDTH;  x++) {
+        for (var y = 0; y < TEMPLATE_HEIGHT;  y++) {
+            if (this.SPIRITS[spirit.shape][spirit.rotation][y].charCodeAt(x) != BLANK) {
+                this.graphics.fillRect(px+x*BOX_SIZE, py+y*BOX_SIZE, BOX_SIZE, BOX_SIZE);
+            }
+        }
+    }
+    var shadowAddY = 0;
+    while(this.isValidPosition(spirit, 0, shadowAddY + 1)) {
+        shadowAddY += 1;
+    }
+    py = py + shadowAddY*BOX_SIZE;
+    this.graphics.strokeStyle = this.clolorMap[color];
+    for (var x = 0; x < TEMPLATE_WIDTH;  x++) {
+        for (var y = 0; y < TEMPLATE_HEIGHT;  y++) {
+            if (this.SPIRITS[spirit.shape][spirit.rotation][y].charCodeAt(x) != BLANK) {
+                this.graphics.strokeRect(px+x*BOX_SIZE, py+y*BOX_SIZE, BOX_SIZE, BOX_SIZE);
+            }
+        }
+    }
 }
 
 board.prototype.clearScreen = function() {
-	this.graphics.fillStyle = "#000000";
-	this.graphics.fillRect(10, 10, 
-		(this.boardWidth+1)*BOX_SIZE+BOARD_MARGIN, (this.boardHeight+1)*BOX_SIZE+BOARD_MARGIN);
+    this.graphics.fillStyle = "#000000";
+    this.graphics.fillRect(10, 10,
+        (this.boardWidth+1)*BOX_SIZE+BOARD_MARGIN, (this.boardHeight+1)*BOX_SIZE+BOARD_MARGIN);
+}
+
+
+board.prototype.updateDisplay = function() {
+    // window.requestAnimationFrame();
+    this.clearScreen();
+    this.drawBoard();
+    if (this.fallingSpirit != null) {
+        this.drawSpirit(this.fallingSpirit);
+    }
+    this.drawBoardLine();
+}
+
+board.prototype.gameLoop = function() {
+    if (this.gamePaused || this.gameOver) {
+        return false;
+    }
+    if (this.fallingSpirit == null) {
+        this.fallingSpirit = this.newSpirit();
+        if(!this.isValidPosition(this.fallingSpirit, 0, 0)) { // game over
+            this.updateDisplay();
+            this.gameOver = true;
+            return true;
+        }
+    }
+
+    if (this.movingLeft && this.isValidPosition(this.fallingSpirit, -1, 0)) {
+        this.fallingSpirit.x -= 1;
+    }
+    if (this.movingRight && this.isValidPosition(this.fallingSpirit, 1, 0)) {
+        this.fallingSpirit.x += 1;
+    }
+    if (this.movingDown && this.isValidPosition(this.fallingSpirit, 0, 1)) {
+        this.fallingSpirit.y += 1;
+    }
+
+    if(!this.isValidPosition(this.fallingSpirit, 0, 1)) {
+        this.addToBoard(this.fallingSpirit);
+        this.score += this.removeCompleteLines();
+        this.fallingSpirit = null;
+    } else {
+        this.fallingSpirit.y += 1;
+    }
+
+    this.updateDisplay();
+    return false;
+}
+
+function loop() {
+  if (workBoard.gameLoop()) {
+    return;
+  }
+  window.setTimeout("loop()", interval);
 }
 
 board.prototype.runGame = function() {
-	if (this.fallingSpirit == null) {
-		this.fallingSpirit = this.newSpirit();
-	}
-	if(!this.isValidPosition(this.fallingSpirit, 0, 1)) {
-		this.addToBoard(this.fallingSpirit);
-		this.fallingSpirit = null;
-	} else {
-		this.fallingSpirit.y += 1;
-	}
-	
-    // window.requestAnimationFrame();
-	this.clearScreen();
-	if (this.fallingSpirit != null) {
-		this.drawSpirit(this.fallingSpirit);
-	}
-	this.drawBoard();
+  loop();
 }
 
 board.prototype.newSpirit = function() {
@@ -215,7 +277,7 @@ board.prototype.newSpirit = function() {
   spirit.shape = shape;
   spirit.rotation = Math.floor(Math.random()*this.SPIRITS[shape].length);
   spirit.x = Math.floor(this.boardWidth / 2) - Math.floor(TEMPLATE_WIDTH / 2);
-  spirit.y = -2;
+  spirit.y = -5;
   spirit.color = Math.floor(Math.random()*this.clolorMap.length);
   return spirit;
 }
@@ -229,36 +291,109 @@ board.prototype.isInMap = function(x, y) {
 }
 
 board.prototype.isValidPosition = function(spirit, addX, addY) {
-	for (var x = 0; x < TEMPLATE_WIDTH;  x++) {
-		for (var y = 0; y < TEMPLATE_HEIGHT;  y++) {
-			if ((y + spirit.y + addY < 0) || 
-					(this.SPIRITS[spirit.shape][spirit.rotation][y].charCodeAt(x) == BLANK)) {
-				continue;
-			}
-			if(!this.isInMap(x + spirit.x + addX, y + spirit.y + addY)) {
-				return false;
-			}
-			if (this.boardMap[x + spirit.x + addX][y + spirit.y + addY] != BLANK) {
-				return false;
-			}
-		}
-	}
-	return true;
+    for (var x = 0; x < TEMPLATE_WIDTH;  x++) {
+        for (var y = 0; y < TEMPLATE_HEIGHT;  y++) {
+            if ((y + spirit.y + addY < 0) ||
+                    (this.SPIRITS[spirit.shape][spirit.rotation][y].charCodeAt(x) == BLANK)) {
+                continue;
+            }
+            if(!this.isInMap(x + spirit.x + addX, y + spirit.y + addY)) {
+                return false;
+            }
+            if (this.boardMap[x + spirit.x + addX][y + spirit.y + addY] != BLANK) {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+board.prototype.moveLeft = function() {
+ if(this.fallingSpirit == null){
+        return;
+  }
+  if(this.isValidPosition(this.fallingSpirit, -1, 0)) {
+        this.fallingSpirit.x -= 1;
+        this.movingLeft = true;
+        this.updateDisplay();
+  }
+}
+
+board.prototype.moveRight = function() {
+  if(this.fallingSpirit == null){
+        return;
+  }
+  if(this.isValidPosition(this.fallingSpirit, 1, 0)) {
+        this.fallingSpirit.x += 1;
+        this.movingRight = true;
+        this.updateDisplay();
+  }
+}
+
+board.prototype.moveDown = function() {
+    if(this.fallingSpirit == null){
+        // crate a new one fast
+        this.gameLoop();
+        return;
+    }
+    if(this.isValidPosition(this.fallingSpirit, 0, 1)) {
+        this.fallingSpirit.y += 1;
+        this.movingDown = true;
+        this.updateDisplay();
+    }
+}
+
+board.prototype.rotateRight = function() {
+    if(this.fallingSpirit == null){
+        return;
+    }
+    var spiritLen = this.SPIRITS[this.fallingSpirit.shape].length;
+    this.fallingSpirit.rotation = (this.fallingSpirit.rotation + 1) % spiritLen;
+    if(!this.isValidPosition(this.fallingSpirit, 0, 0)) {
+        this.fallingSpirit.rotation = (this.fallingSpirit.rotation - 1) % spiritLen;
+    } else {
+        this.updateDisplay();
+    }
 }
 
 board.prototype.addToBoard = function(spirit) {
-	for (var x = 0; x < TEMPLATE_WIDTH;  x++) {
-		for (var y = 0; y < TEMPLATE_HEIGHT;  y++) {
-			if (this.SPIRITS[spirit.shape][spirit.rotation][y].charCodeAt(x) != BLANK) {
-				this.boardMap[x + spirit.x][y + spirit.y] = spirit.color;
-			}
-		}
-	}
+    for (var x = 0; x < TEMPLATE_WIDTH;  x++) {
+        for (var y = 0; y < TEMPLATE_HEIGHT;  y++) {
+            if (this.SPIRITS[spirit.shape][spirit.rotation][y].charCodeAt(x) != BLANK) {
+                this.boardMap[x + spirit.x][y + spirit.y] = spirit.color;
+            }
+        }
+    }
 }
 
 
-board.prototype.isCompleteLine = function() {
+board.prototype.isCompleteLine = function(y) {
+  for (var x = 0; x < this.boardWidth; x++) {
+    if (this.boardMap[x][y] == BLANK) {
+      return false;
+    }
+  }
+  return true;
 }
 
-board.prototype.removeCompleteSpirit = function() {
+board.prototype.removeCompleteLines = function() {
+    var numLinesRemoved = 0;
+    var y = this.boardHeight - 1;
+    while (y >= 0) {
+        if (this.isCompleteLine(y)) {
+            for(var pullDownY = y; pullDownY > 0; pullDownY--) {
+                for (var x = 0; x < this.boardWidth; x++) {
+                    this.boardMap[x][pullDownY] = this.boardMap[x][pullDownY-1];
+                }
+            }
+            // set top line to BLANK
+            for (var x = 0; x < this.boardWidth; x++) {
+                this.boardMap[x][0] = BLANK;
+            }
+            numLinesRemoved += 1;
+         } else {
+            y -= 1;
+         }
+    }
+    return numLinesRemoved;
 }
--- main.js
+++ main.js
@@ -1,22 +1,58 @@
-var FPS = 1;
 var workBoard = null;
 
-function moveBox() {
-  
+function keydownCallback(e) {
+    switch(e.keyCode) {
+        case 37: // <-
+        case 65: // a
+            workBoard.moveLeft();
+            break;
+        case 39: // ->
+        case 68: // d
+            workBoard.moveRight();
+            break;
+        case 40: // down
+        case 83: // s
+            workBoard.moveDown();
+            break;
+        case 38: // up 
+        case 87: // w
+            workBoard.rotateRight();
+            break;
+        case 32: // space
+            if (workBoard.gamePaused) {
+                workBoard.gamePaused = false;
+            } else {
+                workBoard.gamePaused = true;
+            }
+            break;
+    }
+}
+
+function keyupCallback(e) {
+    switch(e.keyCode) {
+        case 37: // <-
+        case 65: // a
+            workBoard.movingLeft = false;
+            break;
+        case 39: // ->
+        case 68: // d
+            workBoard.movingRight = false;
+            break;
+        case 40: // down
+        case 83: // s
+            workBoard.movingDown = false;
+            break;
+    }
 }
 
 function main() {
-	var myCanvas = document.getElementById("myCanvas");
-	var ctx = myCanvas.getContext("2d");
+    var myCanvas = document.getElementById("myCanvas");
+    var ctx = myCanvas.getContext("2d");
 
-	myCanvas.addEventListener("keydown", moveBox, false);
-  var myBoard = new board(10, 20, ctx);
-  myBoard.init();
-  workBoard = myBoard;
-  setInterval("loop()", 1000/FPS);
-  //loop();
-}
-
-function loop() {
-  workBoard.runGame();
+    document.addEventListener("keydown", keydownCallback, false);
+    document.addEventListener("keyup", keyupCallback, false);
+    var myBoard = new board(10, 20, ctx);
+    workBoard = myBoard;
+    myBoard.init();
+    myBoard.runGame();
 }
