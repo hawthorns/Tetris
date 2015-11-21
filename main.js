4c1c62a4d9b8bfe0af813280d8d78f5613ec01c5
kerry <kerrysz.sk@gmail.com>
2015-11-21 13:55:09 +0800

    demo

--- main.js
+++ main.js
@@ -51,12 +51,46 @@ function keyupCallback(e) {
 	e.preventDefault();
 }
 
+var startX = 0;
+var startY = 0;
+function touchStart(e) {
+	e.preventDefault();
+    startX = e.targetTouches[0].pageX,
+    startY = e.targetTouches[0].pageY;
+}
+
+function touchMove(e) {
+	e.preventDefault();
+    var curX = event.targetTouches[0].pageX - startX;
+    var curY = event.targetTouches[0].pageY - startY;
+    if ( Math.abs(curX) > Math.abs(curY) && curX > 0 ) {
+        workBoard.moveRight();
+		workBoard.movingRight = false;
+    }
+    else if ( Math.abs(curX) > Math.abs(curY) && curX < 0 ) {
+        workBoard.moveLeft();
+		workBoard.movingLeft = false;
+    }
+    else if ( Math.abs(curY) > Math.abs(curX) && curY > 0) {
+        workBoard.moveDown();
+		workBoard.movingDown = false;
+    }
+    else if ( Math.abs(curY) > Math.abs(curX) && curY < 0 ) {
+        workBoard.rotateRight();
+    }
+    else{
+        //alert("just touch");
+    }
+}
+
 function main() {
     var myCanvas = document.getElementById("myCanvas");
     var ctx = myCanvas.getContext("2d");
 
     document.addEventListener("keydown", keydownCallback, false);
-    document.addEventListener("keyup", keyupCallback, false);
+	document.addEventListener("keyup", keyupCallback, false);
+    myCanvas.addEventListener("touchstart", touchStart, false);
+    myCanvas.addEventListener("touchmove", touchMove, false);
     var myBoard = new board(10, 20, ctx);
     workBoard = myBoard;
     myBoard.init();
